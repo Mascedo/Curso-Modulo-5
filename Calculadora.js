898fb33b5018565c67c89af8de2c6da09cc87bbf
americo macedo <americomascedo@gmail.com>
2025-02-12 17:49:53 -0300

    calculadora normal completa

--- Calculadora.js
+++ Calculadora.js
@@ -1,10 +1,13 @@
 const prompt = require('prompt-sync')();
 console.log(' --Esta é a calculadora CAUCULADA--')
+let num1 = 0
+let op = ''
+let num2 = 0
 numeros()
 function numeros(){
-let num1 = Number(prompt("qual o primeiro numero da operação? "))
-let op = prompt("qual operação a fazer(use +,-,*,/,%):")
-let num2 = Number(prompt("qual o segundo numero da operação? "))
+num1 = Number(prompt("qual o primeiro numero da operação? "))
+op = prompt("qual operação a fazer(use +,-,*,/,%):")
+num2 = Number(prompt("qual o segundo numero da operação? "))
 if(isNaN(num1)||isNaN(num2)){
     console.log("por favor, use numeros!")
     numeros()
@@ -30,4 +33,19 @@ if(isNaN(num1)||isNaN(num2)){
         numeros()
     }
 }
+}
+function adicionar(){
+    console.log('o resultado de '+ num1 + op + num2 + ' é ' + (num1+num2))
+}
+function subtrair(){
+    console.log('o resultado de '+ num1 + op + num2 + ' é ' + (num1-num2))
+}
+function multiplicar(){
+    console.log('o resultado de '+ num1 + op + num2 + ' é ' + (num1*num2))
+}
+function dividir(){
+    console.log('o resultado de '+ num1 + op + num2 + ' é ' + (num1/num2))
+}
+function porcetagem(){
+    console.log(num2 + '% de '+ num1 + ' é ' + (num1*(num2/100)))
 }
