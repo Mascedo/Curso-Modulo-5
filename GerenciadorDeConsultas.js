1f5211a60e0104b1e5957ba11a79a1d3b7f482e0
americo macedo <americomascedo@gmail.com>
2025-02-13 18:45:16 -0300

    terminei o menu

--- GerenciadorDeConsultas.js
+++ GerenciadorDeConsultas.js
@@ -1,1 +1,34 @@
 const prompt = require('prompt-sync')();
+
+
+menu()
+function menu(){
+    console.log('---Gerenciador de consultas---')
+    console.log('1.Adicionar consulta')
+    console.log('2.Mostrar consulta')
+    console.log('3.Atualizar consulta')
+    console.log('4.Remover consulta')
+    console.log('5.Sair')
+    let escolha = Number(prompt('Digite o numero correspodente a ação:'))
+
+switch(escolha){
+    case 1:
+        add()
+        break;
+    case 2:
+        show()
+        break;
+    case 3:
+        att()
+        break;
+    case 4:
+        del()
+        break;
+    case 5:
+        console.log('Obrigado por usar o gerenciador de consultas!')
+        process.exit()
+    default:
+        console.log('---USE OS NUMEROS RESPECTIVOS---')
+        menu()
+}
+}
